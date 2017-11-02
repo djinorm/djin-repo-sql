ecfad956bc80a036aa83cddc996b86bf47249f6f
XAKEPEHOK <icq-475204@ya.ru>
2017-11-02 15:03:41 +0300

    Djin 2.5.0

--- tests/MySQL/Mappers/DatetimeMapperTest.php
+++ tests/MySQL/Mappers/DatetimeMapperTest.php
@@ -7,6 +7,7 @@
 
 namespace DjinORM\Repositories\Sql\MySQL\Mappers;
 
+use DateTimeImmutable;
 use DjinORM\Djin\Exceptions\ExtractorException;
 use DjinORM\Djin\Exceptions\HydratorException;
 use DjinORM\Djin\TestHelpers\MapperTestCase;
@@ -17,10 +18,10 @@ class DatetimeMapperTest extends MapperTestCase
     public function testHydrate()
     {
         $this->assertHydrated(null, null, $this->getMapperAllowNull());
-        $this->assertHydrated('', null, $this->getMapperAllowNull());
-        $this->assertHydrated('2017-11-02', new \DateTimeImmutable('2017-11-02'), $this->getMapperAllowNull());
-        $this->assertHydrated('2017-11-02 11:43', new \DateTimeImmutable('2017-11-02 11:43'), $this->getMapperAllowNull());
-        $this->assertHydrated('2017-11-02 11:43:15', new \DateTimeImmutable('2017-11-02 11:43:15'), $this->getMapperAllowNull());
+        $this->assertHydrated(null, '', $this->getMapperAllowNull());
+        $this->assertHydrated(new DateTimeImmutable('2017-11-02'), '2017-11-02', $this->getMapperAllowNull());
+        $this->assertHydrated(new DateTimeImmutable('2017-11-02 11:43'), '2017-11-02 11:43', $this->getMapperAllowNull());
+        $this->assertHydrated(new DateTimeImmutable('2017-11-02 11:43:15'), '2017-11-02 11:43:15', $this->getMapperAllowNull());
 
         $this->expectException(HydratorException::class);
         $this->assertHydrated(null, null, $this->getMapperDisallowNull());
@@ -29,10 +30,10 @@ class DatetimeMapperTest extends MapperTestCase
     public function testExtract()
     {
         $this->assertExtracted(null, null, $this->getMapperAllowNull());
-        $this->assertExtracted('', null, $this->getMapperAllowNull());
-        $this->assertExtracted(new \DateTimeImmutable('2017-11-02'), '2017-11-02 00:00:00', $this->getMapperAllowNull());
-        $this->assertExtracted(new \DateTimeImmutable('2017-11-02 11:43'), '2017-11-02 11:43:00', $this->getMapperAllowNull());
-        $this->assertExtracted(new \DateTimeImmutable('2017-11-02 11:43:15'), '2017-11-02 11:43:15', $this->getMapperAllowNull());
+        $this->assertExtracted(null, '', $this->getMapperAllowNull());
+        $this->assertExtracted('2017-11-02 00:00:00', new DateTimeImmutable('2017-11-02'), $this->getMapperAllowNull());
+        $this->assertExtracted('2017-11-02 11:43:00', new DateTimeImmutable('2017-11-02 11:43'), $this->getMapperAllowNull());
+        $this->assertExtracted('2017-11-02 11:43:15', new DateTimeImmutable('2017-11-02 11:43:15'), $this->getMapperAllowNull());
 
         $this->expectException(ExtractorException::class);
         $this->assertExtracted(null, null, $this->getMapperDisallowNull());
